e4349764f3af6936f1cdc35fee05753a574b0d13
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-01-15 20:57:40 +1100

    Added some UI effects

--- paper-scissors-rock.js
+++ paper-scissors-rock.js
@@ -1,48 +1,53 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
+// var userChoice = prompt("Do you choose rock, paper or scissors?");
+// var computerChoice = Math.random();
 
 var resultTextBox = document.getElementById('result');
 var computerResult = document.getElementById('computer');  
 var userResult = document.getElementById('user');  
 
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if (computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
-}
-console.log("User    : " + userChoice);
-console.log("Computer: " + computerChoice);
+function play() {
+    var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var computerChoice = Math.random(); 
 
-var compare = function (choice1, choice2) {
-    if (choice1 === choice2) {
-        return "The result is a tie!"
-    } else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            return "rock wins!"
-        } else {
-            return "paper wins!"
-        }
-    } else if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            return "paper wins!"
-        } else {
-            return "scissors wins!"
-        }
-    } else if (choice1 === "scissors") {
-        if (choice2 === "rock") {
-            return "rock wins!"
-        } else {
-            return "scissors wins!"
-        }
+    if (computerChoice < 0.34) {
+        computerChoice = "rock";
+    } else if (computerChoice <= 0.67) {
+        computerChoice = "paper";
     } else {
-        return "Invalid choice '" + choice1 + "'";
+        computerChoice = "scissors";
     }
-};
-var result = compare(userChoice, computerChoice);
+    console.log("User    : " + userChoice);
+    console.log("Computer: " + computerChoice);
+    
+    var compare = function (choice1, choice2) {
+        choice1 = choice1.toLowerCase()
+        if (choice1 === choice2) {
+            return "The result is a tie!"
+        } else if (choice1 === "rock") {
+            if (choice2 === "scissors") {
+                return "Rock wins!"
+            } else {
+                return "Paper wins!"
+            }
+        } else if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                return "Paper wins!"
+            } else {
+                return "Scissors wins!"
+            }
+        } else if (choice1 === "scissors") {
+            if (choice2 === "rock") {
+                return "Rock wins!"
+            } else {
+                return "Scissors wins!"
+            }
+        } else {
+            return "Invalid choice '" + choice1 + "'";
+        }
+    };
+    var result = compare(userChoice, computerChoice);
 
-// Displaying result on web page
-resultTextBox.innerHTML += result;
-userResult.innerHTML = userChoice;
-computerResult.innerHTML = computerChoice;
+    resultTextBox.innerHTML = 'Result: ' + result;
+    userResult.innerHTML = 'You chose ' + userChoice;
+    computerResult.innerHTML = 'The computer chose ' + computerChoice;
+}
